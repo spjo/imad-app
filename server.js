f4b26330c3a924b63c097214eef9322d81d6495a
spjo <30810201+spjo@users.noreply.github.com>
2017-08-27 14:13:47 +0200

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,6 +5,61 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+var site1 = {
+    title: 'Site1 | Json';
+    heading: 'Site1 B';
+    date: '2017, Aug 24';
+    content:`
+        <p>
+            This is content B
+            fsfasf sf asf dsfas dfas fas dfasdf dsf sdfas fasdfasff
+        </p>
+        <p>
+            sum mo code 
+           slflaslflöasklfjkljasljfljlsklfjösaklfklösdafklöasklfjlkasjlkfjklsjdflslöjklas
+        </p>
+        <p>bib
+        </p>
+        <p>
+            Last content fo now B
+        </p>`
+};
+
+function createTemplate (data){
+    var title = data.title;
+    var heading = data.heading;
+    var date = data.date;
+    var content = data.content;
+    var htmlTemplate= `
+        <html>
+            <head>
+                <title>
+                    ${title}
+                </title>
+                <meta name="viewport" content="width=device-width, initial-sclae=1" />
+                <link href="/ui/style.css" rel="stylesheet" />
+            </head>
+            <body>
+                <div>
+                    <a href="/">Home</a>
+                </div>
+                <hr/>
+                <h5>
+                    ${heading}
+                </h5>
+                <div>
+                    ${date}
+                </div>
+                <div>
+                    ${content}
+                </div>
+            </body>
+        </html>
+        `;
+        return htmlTemplate;
+};
+
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
@@ -22,7 +77,7 @@ app.get('/bibbas', function(req, res){
 });
 
 app.get('/site1', function(req, res){
-  res.sendFile(path.join(__dirname, 'ui', 'site1.html'));
+  res.send(createTemplate(site1));
 });
 
 app.get('/site2', function(req, res){
