68fc44b4dcb11a89f3c94b0efb316a19f872577f
spjo <30810201+spjo@users.noreply.github.com>
2017-08-27 17:33:51 +0200

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -112,6 +112,7 @@ app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
+
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
