ea20b1e3c34d8cd55207b93ba1fcd3eba1f20302
spjo <30810201+spjo@users.noreply.github.com>
2017-08-28 14:01:18 +0200

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,7 +2,21 @@
 var button = document.getElementById('counter');
 var counter = 0;
 button.onclick = function() {
-    counter++;
-    var span = document.getElementById('count');
-    span.innerHTML = counter.toString();
+    
+    var request = new XMLHttpRequest();
+    
+    
+    request.onreadystatechange = function(){
+        if(request.readyState=== XMLHttpRequest.DONE){
+            if(request.status==200){
+                var counter = request.responseText;
+                var span = document.getElementById('count');
+                span.innerHTML = counter.toString();
+            }
+        }
+        
+    };
+    
+    request.open('GET', 'http://jonatansprick85.imad.hasura-app.io/counter', true);
+    request.send(null);
 };
