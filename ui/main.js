0ddf2aa6d7b25dc84fdabd398fcfccf3d5f61f24
spjo <30810201+spjo@users.noreply.github.com>
2017-08-28 13:51:59 +0200

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,8 +1,7 @@
 
-var counter = 0;
 var button = document.getElementById('counter');
+var counter = 0;
 button.onClick = function() {
-    
     counter++;
     var span = document.getElementById('count');
     span.innerHTML = counter.toString();
