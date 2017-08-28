8f5dd4c479f7ffadb051e47263ec1ef80f5a4e85
spjo <30810201+spjo@users.noreply.github.com>
2017-08-28 13:49:35 +0200

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,19 +1,3 @@
-console.log('Loaded!');
-
-document.getElementById('main-text').innerHTML = "new Text";
-
-var pic = document.getElementById('madi');
-
-var marginLeft = 10;
-function moveRight (){
-    marginLeft += 3;
-    pic.style.marginLeft = marginLeft + 'px';
-}
-
-pic.onclick = function(){
-    var interval = setInterval(moveRight, 30);
-//    pic.style.marginLeft = '200px';
-};
 
 var counter = 0;
 var button = document.getElementById('counter');
